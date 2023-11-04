c1496d7cc9b82a188c2c114ad1084b356bb9cd84
Rasti1 <rostrostov1999@gmail.com>
2023-11-04 11:15:47 +0200

    Fixed commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,21 +1,30 @@
 package app;
+
 import java.util.Scanner;
 public class Main {
+
+    static double taxRate;
     static double tax;
     static double income;
+
     public static void main(String[] args) {
+
        Scanner sc = new Scanner(System.in);
        System.out.print("Input your income: ");
        income = sc.nextDouble();
-       if (income <= 10000){
-           tax = income * 0.025;
-       }
-       else if (income > 10000 && income <= 25000 ){
-           tax = income * 0.043;
-       }
-       else {
-           tax = income * 0.067;
+       if (income < 0){
+          System.out.println("Wrong income!");
+       } else if (income <= 10000){
+           taxRate = 2.5;
+           tax = income * taxRate / 100;
+       } else if (income > 10000 && income <= 25000 ){
+           taxRate = 4.7;
+           tax = income * taxRate / 100;
+       } else {
+           taxRate = 6.7;
+           tax = income * taxRate / 100;
        }
        System.out.printf("Tax: %.2f%n", tax);
+
     }
 }
